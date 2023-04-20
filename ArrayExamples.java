01b259acc4459a004f894d5df8ac75c69f9b2587
tayaoyoyoyo <tayaoemma@gmail.com>
2023-04-20 14:34:44 -0700

    fixed bugs in reversed and reversedinplace

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,15 +4,24 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    int temp;
+    for(int i = 0; i < arr.length/2; i += 1) {
+      temp = arr[i];
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = temp;
     }
   }
 
   // Returns a *new* array with all the elements of the input array in reversed
   // order
   static int[] reversed(int[] arr) {
+
     int[] newArray = new int[arr.length];
+    // creates a deep copy of arr 
+    for (int i = 0; i < arr.length; i++){
+      newArray[i] = arr[i];
+    }
+
     for(int i = 0; i < arr.length; i += 1) {
       arr[i] = newArray[arr.length - i - 1];
     }
--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReversedInPlace2(){
+    int[] input1 = { 1,2,3 };
+    assertArrayEquals( new int[]{ 3,2,1 }, ArrayExamples.reversed(input1));
+  }
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReveresedNonEmpty(){
+    int[] input1 = { 1,2,3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals( new int[]{ 3,2,1 }, input1);
   }
 }
